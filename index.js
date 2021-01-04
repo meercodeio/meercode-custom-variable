f5adc917fb9ef8f1e1cf61ecb0ba79d747111137
Omer Faruk APLAK <omeraplak@gmail.com>
2021-01-04 09:53:40 +0300

    v0.0.1

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+async function main() {
+    try {
+        const meercodeToken = core.getInput('meercode-token');
+        const userDefinedURL = core.getInput('url');
+
+        const runId = process.env.GITHUB_RUN_ID;
+        
+        const url = 'https://develop.api.meercode.io/api/run';
+
+        const config = {
+            url,
+            method: 'PATCH',
+            headers: {
+                "content-type": "application/json",
+                "Authorization": meercodeToken
+            },
+            data: {
+                runId,
+                url: userDefinedURL,
+            }
+        };
+
+        const response = await axios(config);
+
+        console.log(response);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+main();
